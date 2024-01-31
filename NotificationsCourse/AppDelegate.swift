0e4d79ab3282b7610c4c6c3ba5d593a7daabc9ed
callmeapplee <boturgithub@gmail.com>
2024-01-31 15:08:32 +0500

    feat(UNUserNotificationCenterDelegate): working with UNUserNotifiactionCenterDelegate.

--- NotificationsCourse/AppDelegate.swift
+++ NotificationsCourse/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import UserNotificationsg
+import UserNotifications
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     let notificationCenter = UNUserNotificationCenter.current()
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         requestAutorization()
+        notificationCenter.delegate = self
         return true
     }
 
@@ -52,4 +53,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
-
+extension AppDelegate:UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.sound,.alert])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.notification.request.identifier == "Local Notification" {
+            print("Handling notification with the Local Notifiaction Identifier")
+        }
+        completionHandler()
+    }
+}
